f7d2320183097238a28970723f21354d29ed641e
sultaAhad <sultan.ahad@inoviotech.com>
2024-05-30 21:07:27 +0500

    add

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,42 +1,82 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { logo } from "../images/index";
 import "bootstrap/dist/css/bootstrap.css";
 import "../assets/css/Style.css";
+import Hamburger from "hamburger-react";
 
 const Header = () => {
+	const [isOpen, setOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setOpen(!isOpen);
+	};
+
 	return (
-		<section className="header">
-			<Container>
-				<Row className="align-items-center">
-					<Col lg={3} xs={6}>
-						<div className="logo-wrapper">
-							<img src={logo} className="img-fluid" alt="Company Logo" />
-						</div>
-					</Col>
-					<Col lg={6} xs={6}>
-						<div className="navbar-wrapper">
-							<ul>
-								<li>
-									<Link To="/">Home</Link>
-									<Link To="/about">About</Link>
-									<Link To="/faqs">FAQs</Link>
-									<Link To="/contact">Contact</Link>
-								</li>
-							</ul>
-						</div>
-					</Col>
-					<Col lg={3} xs={6}>
-						<div className="btn-wrapper">
-							<Link To="/" className="btn">
-								Sign In
-							</Link>
-						</div>
-					</Col>
-				</Row>
-			</Container>
-		</section>
+		<>
+			<section className="header">
+				<Container>
+					<Row className="align-items-center">
+						<Col lg={3} xs={6}>
+							<div className="logo-wrapper">
+								<img src={logo} className="img-fluid" alt="Company Logo" />
+							</div>
+						</Col>
+						<Col lg={6} xs={6} className="hidden-wrapper">
+							<div className="navbar-wrapper ">
+								<ul>
+									<li>
+										<Link to="/">Home</Link>
+									</li>
+									<li>
+										<Link to="/about">About</Link>
+									</li>
+									<li>
+										<Link to="/faqs">FAQs</Link>
+									</li>
+									<li>
+										<Link to="/contact">Contact</Link>
+									</li>
+								</ul>
+							</div>
+						</Col>
+						<Col
+							lg={3}
+							xs={6}
+							className="position-relative d-flex align-items-center gap-3"
+						>
+							<div className="btn-wrapper">
+								<Link to="/" className="btn">
+									Sign In
+								</Link>
+							</div>
+							<Hamburger toggled={isOpen} toggle={toggleMenu} />
+						</Col>
+					</Row>
+				</Container>
+			</section>
+			<div id="top-nav" className={isOpen ? "" : "hidden"}>
+				<div className="nav-menu">
+					<div className="menu-list navbar-wrapper">
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/about">About</Link>
+							</li>
+							<li>
+								<Link to="/faqs">FAQs</Link>
+							</li>
+							<li>
+								<Link to="/contact">Contact</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -91,6 +91,29 @@ const Home = ({ data }) => {
 		autoplay: true,
 		autoplaySpeed: 2000,
 		cssEase: "ease-in-out",
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+		],
 	};
 	useEffect(() => {
 		const slickActive = document.querySelector(".slick-active");
@@ -111,6 +134,29 @@ const Home = ({ data }) => {
 		autoplay: true,
 		autoplaySpeed: 2000,
 		cssEase: "ease-in-out",
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+		],
 	};
 	const faqData = [
 		{
